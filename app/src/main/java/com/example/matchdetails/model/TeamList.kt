69e0db558468fe5d6be41262153d640cd2274c2a
jayshree097 <jayshreedongre09@gmail.com>
2023-02-17 17:13:45 +0530

    second page response printed

--- app/src/main/java/com/example/matchdetails/model/TeamList.kt
+++ app/src/main/java/com/example/matchdetails/model/TeamList.kt
@@ -4,5 +4,4 @@ import com.google.gson.annotations.SerializedName
 
 data class TeamList(
     @SerializedName("Teams")
-    var teams:  Map<String, ResponseTeam>
-)
+    var teams:  Map<String, ResponseTeam>)
